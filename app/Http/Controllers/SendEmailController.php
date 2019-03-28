668bf7f9ae62ba99b6f638b2e4dfad2005e67209
zerubabbel <achiappetta90@gmail.com>
2019-03-28 11:16:45 -0700

    further building out email

--- app/Http/Controllers/SendEmailController.php
+++ app/Http/Controllers/SendEmailController.php
@@ -9,4 +9,22 @@ class SendEmailController extends Controller
     function index(){
     	return view('contact/send_email');
     }
+
+    function send(Request $request)
+    {
+     $this->validate($request, [
+      'name'     =>  'required',
+      'email'  =>  'required|email',
+      'message' =>  'required'
+     ]);
+
+        $data = array(
+            'name'      =>  $request->name,
+            'message'   =>   $request->message
+        );
+
+     Mail::to('zerubabbel.uidev@gmail.com')->send(new SendMail($data));
+     return back()->with('success', 'Thanks for contacting us!');
+
+    }
 }
